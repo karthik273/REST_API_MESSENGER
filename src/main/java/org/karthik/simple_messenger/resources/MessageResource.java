e99cc8c8435328430ea7792772a4c46e6d3aa039
Karthik <karthikthnr2@gmail.com>
2017-07-01 20:01:13 -0500

    Implemented GET and POST method

--- src/main/java/org/karthik/simple_messenger/resources/MessageResource.java
+++ src/main/java/org/karthik/simple_messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.karthik.simple_messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,15 +18,23 @@ public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages()
 	{
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message)
+	{
+		
+		return messageService.addMessage(message);
+	}
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId")long messageId)
 	{
 		
